d1428a800090b6d22a062762f38a968ddef2d888
liveinsidee <ruslankapralov91@gmail.com>
2024-02-03 14:05:07 +0600

    lз7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useMemo, useCallback } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
 
-export default App;
+  const squaredValue = useMemo(() => {
+    console.log('Вычисление квадрата значения');
+    return count * count;
+  }, [count]);
+
+  const increment = useCallback(() => {
+    console.log('Инкремент значения');
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  return (
+      <div className="counter-container">
+        <p>Текущее значение: {count}</p>
+        <p>Квадрат значения: {squaredValue}</p>
+        <button onClick={increment}>Увеличить значение</button>
+      </div>
+  );
+};
+
+export default CounterComponent;
